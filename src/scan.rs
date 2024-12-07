13e4dc3a4fbcb6f8f96d5b37dfbf5f7dcc1d279a
Ayush Biswas <biswas.ayush03@kgpian.iitkgp.ac.in>
2024-12-07 07:13:17 +0000

    codecrafters submit [skip ci]

--- src/scan.rs
+++ src/scan.rs
@@ -125,7 +125,26 @@ impl Scanner {
                             res.push(c);
                         }
                         let id: String = res.into_iter().collect();
-                        (TokenType::Identifier, id.clone(), Literal::None)
+                        let tok_type = match id.as_str() {
+                            "and" => TokenType::And,
+                            "class" => TokenType::Class,
+                            "else" => TokenType::Else,
+                            "false" => TokenType::False,
+                            "for" => TokenType::For,
+                            "fun" => TokenType::Fun,
+                            "if" => TokenType::If,
+                            "nil" => TokenType::Nil,
+                            "or" => TokenType::Or,
+                            "print" => TokenType::Print,
+                            "return" => TokenType::Return,
+                            "super" => TokenType::Super,
+                            "this" => TokenType::This,
+                            "true" => TokenType::True,
+                            "var" => TokenType::Var,
+                            "while" => TokenType::While,
+                            _ => TokenType::Identifier,
+                        };
+                        (tok_type, id.clone(), Literal::None)
                     } else if c.is_whitespace() {
                         continue;
                     } else {
